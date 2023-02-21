09479ca44dd25fc5a2b3c0135968744a601fac28
Vinay Devareddy <vinaydevareddy1702@gmail.com>
2023-02-21 13:09:10 -0500

    add eslint, fix style errors

--- part3/contactsBackend/index.js
+++ part3/contactsBackend/index.js
@@ -12,20 +12,20 @@ app.use(express.json())
 
 // logging requests
 app.use(morgan((tokens, req, res) => {
-    let data = ""
-    if(req.method === "POST") {
-        data = JSON.stringify(req.body)
-    }
+	let data = ''
+	if (req.method === 'POST') {
+		data = JSON.stringify(req.body)
+	}
 
-    return [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'),
-        '-',
-        tokens['response-time'](req, res), 'ms',
-        data
-    ].join(' ')
+	return [
+		tokens.method(req, res),
+		tokens.url(req, res),
+		tokens.status(req, res),
+		tokens.res(req, res, 'content-length'),
+		'-',
+		tokens['response-time'](req, res), 'ms',
+		data
+	].join(' ')
 }))
 
 
@@ -34,133 +34,132 @@ app.use(morgan((tokens, req, res) => {
 /*** Get Requests ***/
 
 // get home page
-app.get('/', (request, response, next) => {
-    response.send('<h1>Hello World!</h1>')
+app.get('/', (request, response) => {
+	response.send('<h1>Hello World!</h1>')
 })
 
 // get information
 app.get('/info', (request, response, next) => {
-    Contact.find({})
-    .then(contacts => {
-        response.send(`<p>The phonebook has the information of ${contacts.length} people</p>
+	Contact.find({})
+		.then(contacts => {
+			response.send(`<p>The phonebook has the information of ${contacts.length} people</p>
         <p>${Date()}</p>`)
-        mongoose.connection.close()
-    })
-    .catch(error => next(error))
+		})
+		.catch(error => next(error))
 })
 
 // get all contacts
 app.get('/api/contacts', (request, response, next) => {
-    Contact.find({})
-    .then(contacts => {
-        response.json(contacts)
-    })
-    .catch(error => next(error))
+	Contact.find({})
+		.then(contacts => {
+			response.json(contacts)
+		})
+		.catch(error => next(error))
 })
 
 // get contact by id
 app.get('/api/contacts/:id', (request, response, next) => {
-    Contact.findById(request.params.id)
-    .then(contact => {
-        if(contact) {
-            response.json(contact)
-        } else {
-            response.status(404).end()
-        }
-    })
-    .catch(error => next(error))
+	Contact.findById(request.params.id)
+		.then(contact => {
+			if (contact) {
+				response.json(contact)
+			} else {
+				response.status(404).end()
+			}
+		})
+		.catch(error => next(error))
 })
 
 
 /*** Delete Requests ***/
 // delete contact by id
 app.delete('/api/contacts/:id', (request, response, next) => {
-    Contact.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+	Contact.findByIdAndRemove(request.params.id)
+		.then(() => {
+			response.status(204).end()
+		})
+		.catch(error => next(error))
 })
 
 
 /*** Post Requests ***/
 // post new contact
 app.post('/api/contacts', (request, response, next) => {
-    const body = request.body
+	const body = request.body
 
-    // if(!body.name) {
-    //     return response.status(400).json({error: 'missing name'})
-    // }
+	// if(!body.name) {
+	//     return response.status(400).json({error: 'missing name'})
+	// }
 
-    // if(!body.number) {
-    //     return response.status(400).json({error: 'missing number'})
-    // }
+	// if(!body.number) {
+	//     return response.status(400).json({error: 'missing number'})
+	// }
 
-    /**
-     * TODO: check for duplicate name
-     * if duplicate, need to update
-     * otherwise, create new contact
-     */
+	/**
+	 * TODO: check for duplicate name
+	 * if duplicate, need to update
+	 * otherwise, create new contact
+	 */
 
-    // if(contacts.some(contact => contact.name === body.name)) {
-    //     return response.status(400).json({error: 'name must be unique'})
-    // }
+	// if(contacts.some(contact => contact.name === body.name)) {
+	//     return response.status(400).json({error: 'name must be unique'})
+	// }
 
-    const contact = new Contact({
-        name: body.name,
-        number: body.number
-    })
+	const contact = new Contact({
+		name: body.name,
+		number: body.number
+	})
 
-    contact.save()
-    .then(res => {
-        console.log("contact saved!")
-        response.json(contact)
-    })
-    .catch(error => next(error))
+	contact.save()
+		.then(() => {
+			console.log('contact saved!')
+			response.json(contact)
+		})
+		.catch(error => next(error))
 
-    
+
 })
 
 /** Put Requests */
 app.put('/api/contacts/:id', (request, response, next) => {
-    const body = request.body
+	const body = request.body
 
-    const contact = {
-        name: body.name,
-        number: body.number
-    }
-  
-    Contact.findByIdAndUpdate(request.params.id, contact, 
-        { new: true, runValidators: true, context: 'query' })
-      .then(updatedContact => {
-        response.json(updatedContact)
-      })
-      .catch(error => next(error))
-  })
+	const contact = {
+		name: body.name,
+		number: body.number
+	}
+
+	Contact.findByIdAndUpdate(request.params.id, contact,
+		{ new: true, runValidators: true, context: 'query' })
+		.then(updatedContact => {
+			response.json(updatedContact)
+		})
+		.catch(error => next(error))
+})
 
 
 
 // unknown endpoint request
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+	response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
 // error handler
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-  
-    next(error)
-  }
-  
-  app.use(errorHandler)
+	console.error(error.message)
+
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' })
+	} else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message })
+	}
+
+	next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT)
--- part3/contactsBackend/models/Contact.js
+++ part3/contactsBackend/models/Contact.js
@@ -6,39 +6,39 @@ mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URL
 
-console.log('connecting to', url, "...")
+console.log('connecting to', url, '...')
 mongoose.connect(url)
-    .then(result => {
-        console.log("connected!")
-    })
-    .catch(error => {
-        console.log('error connecting to DB: ', error.message)
-    })
+	.then(() => {
+		console.log('connected!')
+	})
+	.catch(error => {
+		console.log('error connecting to DB: ', error.message)
+	})
 
-const contactSchema = new mongoose.Schema( {
-    name: {
-        type: String,
-        minLength: [3,'Name must be at least 3 characters long'],
-        required: [true, 'Name required']
-    },
-    number: {
-        type: String,
-        validate: {
-            validator: v => {
-              return /^(\d{2,3})(-)?(\d{2,3})-?(\d{3,})$/.test(v);
-            },
-            message: props => `${props.value} is not a valid phone number!`
-          },
-        required: [true, 'Phone number required']
-    }
+const contactSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		minLength: [3, 'Name must be at least 3 characters long'],
+		required: [true, 'Name required']
+	},
+	number: {
+		type: String,
+		validate: {
+			validator: v => {
+				return /^(\d{2,3})(-)?(\d{2,3})-?(\d{3,})$/.test(v)
+			},
+			message: props => `${props.value} is not a valid phone number!`
+		},
+		required: [true, 'Phone number required']
+	}
 })
 
 contactSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-      }
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
 })
 
 module.exports = mongoose.model('Contact', contactSchema)
